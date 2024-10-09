981fae2c790c0a799d526146a17899e5ee75ef4c
ALBERTO MARGAGLIA s284061 <s284061@studenti.polito.it>
2024-10-09 18:29:32 +0200

    GUI picking service and next client

--- Server/index.mjs
+++ Server/index.mjs
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { getServices } from "./dao/totemDao.mjs";
+import { getNextCustomer } from "./dao/officerDao.mjs";
 
 // init express
 const app = new express();
@@ -16,6 +18,24 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// ROUTES
+app.get('/api/services',async (req,res)=>{
+    try {
+        let services=await getServices();
+        res.status(200).json(services);
+    } catch (error) {
+        res.status(503).json({ error: error.message });
+    }
+})
+
+app.get('/api/next/:counterId',async (req,res)=>{
+    try {
+        let next=await getNextCustomer(req.params.counterId);
+        res.status(200).json(next);
+    } catch (error) {
+        res.status(503).json({ error: error.message });
+    }
+})
 // activate the server
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
