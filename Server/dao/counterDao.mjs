6e4aa0a755ae20233d2b812d3e4f3da1a7ee03b8
Enrico Gioseffi <s334022@studenti.polito.it>
2024-10-12 19:35:08 +0200

    Updated the approximate time left per ticket in queue divided per service. There is a bug for the correct result and i will continue working from here tomorrow

--- Server/dao/counterDao.mjs
+++ Server/dao/counterDao.mjs
@@ -4,11 +4,11 @@ const db = new sqlite.Database('db/db.sqlite', (err) => {
     if (err) throw err;
 })
 
-export default function CounterDao(){
-    this.getCounters = () => { 
+export default function CounterDao() {
+    this.getCounters = () => {
         return new Promise((resolve, reject) => {
             const sql = "SELECT id FROM  counter";
-            db.all(sql, [], (err,rows)=>{
+            db.all(sql, [], (err, rows) => {
                 if (err) {
                     return reject(err);
                 }
@@ -16,38 +16,38 @@ export default function CounterDao(){
             });
         });
     }
-    
+
     this.getServicesByCounterId = (counterId) => {
-        return new Promise((resolve,reject) => {
+        return new Promise((resolve, reject) => {
             const sql = `
                 SELECT s.id, s.name 
                 FROM service s
                 JOIN counter_service cs ON s.id = cs.service_id
                 WHERE cs.counter_id = ?`;
-            db.all(sql, [counterId], (err,rows)=> {
-                if(err) {
+            db.all(sql, [counterId], (err, rows) => {
+                if (err) {
                     return reject(err);
-                }else{
+                } else {
                     resolve(rows);
                 }
             });
         });
     }
 
-    this.getTicketFromAllCounters = ()=>{
-        return new Promise((resolve,reject) => {
-            const sql = "SELECT id, actual_t_id  FROM  counter"
-            db.all(sql, [], async(err,rows)=> {
-                if(err) {
+    this.getTicketFromAllCounters = () => {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT id, actual_t_id FROM counter"
+            db.all(sql, [], async (err, rows) => {
+                if (err) {
                     return reject(err);
-                }else{
-                    let vet=[];
-                    for (let element of rows){
-                        if (element.actual_t_id!=null){
-                            vet.push({id: element.id, tag: await this.getTicketTag(element.id)})
+                } else {
+                    let vet = [];
+                    for (let element of rows) {
+                        if (element.actual_t_id != null) {
+                            vet.push({ id: element.id, tag: await this.getTicketTag(element.id), min: await this.getTimePerTicketInQueue(element.actual_t_id) })
                         }
-                        else{
-                            vet.push({id: element.id, tag: null}) 
+                        else {
+                            vet.push({ id: element.id, tag: null, min: null })
                         }
                     }
                     resolve(vet);
@@ -55,17 +55,33 @@ export default function CounterDao(){
             });
         });
     }
-    this.getTicketTag = (id) =>{
-        return new Promise((resolve,reject) => {
-            const sql = "SELECT id, s_tag  FROM  ticket WHERE id = ?"
-            db.get(sql, [id], (err,row)=> {
-                if(err) {
+    this.getTicketTag = (id) => {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT id, s_tag FROM ticket WHERE id = ?"
+            db.get(sql, [id], (err, row) => {
+                if (err) {
                     return reject(err);
-                }else{
-                    let string=`${row.s_tag}${row.id}`
+                } else {
+                    let string = `${row.s_tag}${row.id}`
                     resolve(string)
                 }
             });
-        }); 
+        });
+    }
+    this.getTimePerTicketInQueue = async (t_id) => {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT SUM(s.time) as waitingTime,s.tag
+                         FROM ticket t, service s
+                         WHERE t.s_tag = s.tag
+                         AND t.c_id IS NULL
+                         AND s.tag = (SELECT s_tag FROM ticket WHERE id = ?)`;
+            db.get(sql, [t_id], (err, row) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    resolve(row)
+                }
+            });
+        });
     }
 }
